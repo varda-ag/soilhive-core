f81646f3b102495f00a2c76029a08bfc0eb522b7
VardAlb <alberto.arpini@varda.ag>
2025-10-27 16:14:55 +0100

    SP-4580 adde donation page file with proper case

---
+++ frontend/fe-backend/src/pages/Donation.page.tsx
@@ -0,0 +1,12 @@
+import HTMLLayout from "./HTMLLayout";
+
+export default function Donation() {
+  return (
+    <HTMLLayout title="Soilhive - Donation">
+      <div className="donation-page">
+        <h1>Donation</h1>
+        <p>This is the donation page</p>
+      </div>
+    </HTMLLayout>
+  );
+}
