e4f828b074c3e4ae247e4fd1db2ff467e5a9d9a5
svaccari <stefano.vaccari@varda.ag>
2025-10-31 17:15:02 +0100

    Adding tests

--- backend/tests/routes/config.test.ts
+++ backend/tests/routes/config.test.ts
@@ -2,40 +2,52 @@ import request from "supertest";
 import { app } from "../../src/app";
 import { getDataSource } from "../../src/utils/data-source";
 
-describe("GET /config", () => {
-  it("Responds with not found", async () => {
+const ID = "test-config";
+
+describe("Testing /config/{id} routes", () => {
+  it("PUT saves the config", async () => {
+    const data = { customValue: 123.456 };
+    const res = await request(app).put("/config/test-config").send(data);
+    expect(res.statusCode).toBe(200);
+    const row = await getTestConfigFromDB();
+    expect(row).toBeDefined();
+    expect(row!.id).toStrictEqual(ID);
+    expect(row!.data).toStrictEqual(data);
+  });
+
+  it("GET responds with not found", async () => {
     const res = await request(app).get("/config/wrong-id");
     expect(res.statusCode).toBe(404);
   });
 
-  it("Responds with the expected config", async () => {
+  it("GET responds with the expected config", async () => {
     const data = { key: "value" };
-    await createTestConfig(data);
+    await createTestConfigInDB(data);
     const res = await request(app).get("/config/test-config");
     expect(res.statusCode).toBe(200);
     expect(res.body).toStrictEqual(data);
   });
 
-  it("Deletes the config", async () => {
+  it("DELETE removes the config", async () => {
     const data = { key: "value" };
-    await createTestConfig(data);
+    await createTestConfigInDB(data);
     const res = await request(app).delete("/config/test-config");
     expect(res.statusCode).toBe(204);
-    const row = await getTestConfig();
-    expect(row).toBeUndefined();
+    const row = await getTestConfigFromDB();
+    expect(row).toBeNull();
   });
 });
 
-const createTestConfig = async (data: any) => {
+const createTestConfigInDB = async (data: any) => {
   const dataSource = await getDataSource();
   const repo = dataSource.getRepository("JsonStorage");
-  const id = "test-config";
+  const id = ID;
   await repo.save({ id, data });
 };
 
-const getTestConfig = async () => {
+const getTestConfigFromDB = async () => {
   const dataSource = await getDataSource();
   const repo = dataSource.getRepository("JsonStorage");
-  const id = "test-config";
-  await repo.findOneBy({ id });
+  const id = ID;
+  return await repo.findOneBy({ id });
 };
