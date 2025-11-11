ce872dfc2ee13c7c4e56388c6eb2b254b5a272bb
cristbello <cristian.bellomo@varda.ag>
2025-11-11 18:08:49 +0100

    Managed to load a Mapbox raster tileset with maplibre and added mapStyles argument to SoilhiveMap, also proved multiple maps in the same page work

--- frontend/soilhive-app/src/components/SoilhiveMap.jsx
+++ frontend/soilhive-app/src/components/SoilhiveMap.jsx
@@ -1,65 +1,42 @@
-import { GeolocateControl, Map, NavigationControl, ScaleControl, TerrainControl } from 'react-map-gl/maplibre';
+import { GeolocateControl, Map, NavigationControl, ScaleControl, TerrainControl, type StyleSpecification } from 'react-map-gl/maplibre';
 import GeocoderControl from './GeocoderControl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
 
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidmFyZGEtbWFwYm94LWFkbWluIiwiYSI6ImNsZmttZTQweTA5ZTg0M3MwMGlpanplZmsifQ.Tg6f5VFvnSPjGrwV_g6nVg';
+function MapStyleSwitcher() {
+  return (
+    <div>MapStyleSwitcher skeleton</div>
+  );
+}
 
-const MAP_STYLE = {
-  version: 8,
-  sources: {
-    osm: {
-      type: 'raster',
-      tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-      tileSize: 256,
-      attribution: '&copy; OpenStreetMap Contributors',
-      maxzoom: 19
-    },
-    terrainSource: {
-      type: 'raster-dem',
-      // url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-      url: 'https://api.mapbox.com/v4/{tileset_id}/{zoom}/{x}/{y}.png?access_token='+MAPBOX_ACCESS_TOKEN,
-      tileSize: 256
-    }
-  },
-  layers: [
-    {
-      id: 'osm',
-      type: 'raster',
-      source: 'osm'
-    }
-  ],
-  terrain: {
-    source: 'terrainSource',
-    exaggeration: 1
-  },
-  sky: {}
+interface SoilhiveMapProps {
+  initialViewBoundingBox?: [number, number, number, number],
+  showGeocoder?: boolean,
+  mapStyles?: Array<{ name: string, mapStyle: StyleSpecification }>
 };
 
-
-function SoilhiveMap({initialViewBoundingBox, showGeocoder = false}: {initialViewBoundingBox?: [number, number, number, number], showGeocoder?: boolean}) {
+function SoilhiveMap({
+  initialViewBoundingBox,
+  showGeocoder = false,
+  mapStyles = [{name: 'CartoCDN Basemaps: Voyager', mapStyle: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'}]
+}: SoilhiveMapProps) {
   return (
-    <Map
-      {...(initialViewBoundingBox ? { initialViewState: { bounds: initialViewBoundingBox } } : {})}
-      // initialViewState={{
-      //   bounds: [32.95418, 3.42206, 47.78942, 14.95943]
-      //   // longitude: -100,
-      //   // latitude: 40,
-      //   // zoom: 3.5,
-      // }} 
-      style={{width: "100%"}}
-      // mapStyle="mapbox://styles/mapbox/standard"
-      // mapStyle="mapbox://styles/mapbox/standard-satellite"
-      // mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+    <div className="soilhive-map" style={{ width: '100%', height: "100%" }}>
+      <Map
+        style={{ width: "100%" }}
+        mapStyle={mapStyles[0].mapStyle}
       // mapStyle="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-      mapStyle={MAP_STYLE}
-    >
-      { showGeocoder && <GeocoderControl position="top-left" onLoading={() => {}} onResults={() => {}} onResult={() => {}} onError={() => {}} /> }
-      <GeolocateControl position="bottom-right" />
-      <NavigationControl position="bottom-right"  showCompass={false} showZoom={true} visualizePitch={true} />
-      <ScaleControl />
-      <TerrainControl />
-    </Map>
+      // mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
+      // mapStyle="https://demotiles.maplibre.org/globe.json"
+      // mapStyle="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+      >
+        {showGeocoder && <GeocoderControl position="top-left" onLoading={() => { }} onResults={() => { }} onResult={() => { }} onError={() => { }} />}
+        <GeolocateControl position="bottom-right" />
+        <NavigationControl position="bottom-right" showCompass={false} showZoom={true} visualizePitch={false} />
+        <ScaleControl />
+      </Map>
+      <MapStyleSwitcher />
+    </div>
   );
 };
 
