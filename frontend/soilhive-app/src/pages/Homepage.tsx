d0f026a76687fedcf1dac13201220fcfd46c59a8
cristbello <153092107+cristbello@users.noreply.github.com>
2025-11-13 10:01:07 +0100

    SP-4582 SoilhiveMap implementation (#10)
    * SP-4582 Added MapStyleSwitcher to SoilhiveMap (cannot change the map yet)
    
    * Finished implementation of multiple maps choice and added other properties to SoilhiveMap to change its behaviour
    
    * Removed MAPBOX_ACCESS_TOKEN environment variable from code

--- frontend/soilhive-app/src/pages/Homepage.tsx
+++ frontend/soilhive-app/src/pages/Homepage.tsx
@@ -1,7 +1,6 @@
 import type { StyleSpecification } from "react-map-gl/maplibre";
 import SoilhiveMap from "../components/SoilhiveMap";
-
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidmFyZGEtbWFwYm94LWFkbWluIiwiYSI6ImNsZmttZTQweTA5ZTg0M3MwMGlpanplZmsifQ.Tg6f5VFvnSPjGrwV_g6nVg';
+import { MAPBOX_ACCESS_TOKEN } from "../utilities/environmentVariables";
 
 const MAPBOX_SATELLITE_MAP_STYLE: StyleSpecification = {
   version: 8,
@@ -18,9 +17,7 @@ const MAPBOX_SATELLITE_MAP_STYLE: StyleSpecification = {
     {
       id: "osm-tiles",
       type: "raster",
-      source: "raster-tiles",
-      // minzoom: 0,
-      // maxzoom: 19
+      source: "raster-tiles"
     }
   ]
 };
@@ -30,9 +27,17 @@ function Homepage() {
     <div className="home-page">
       <h1>Homepage</h1>
       <p style={{color: 'var(--primary-color)'}}>This is the homepage</p>
-      <SoilhiveMap />
+      <SoilhiveMap showNavigation={false} showGeolocation={false} showScale={false} scrollZoom={false} dragPan={false} />
       <p>Altra mappa</p>
-      <SoilhiveMap initialViewBoundingBox={[6.6272658, 35.2889616, 18.7844746, 47.0921462]} showGeocoder={true} mapStyles={[{name: 'Mapbox Satellite', mapStyle: MAPBOX_SATELLITE_MAP_STYLE}]}/>
+      <SoilhiveMap
+        initialViewBoundingBox={[6.6272658, 35.2889616, 18.7844746, 47.0921462]} showGeocoder={true}
+        mapStyles={[
+          {name: 'CartoCDN Voyager', mapStyle: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'},
+          {name: 'Mapbox Satellite', mapStyle: MAPBOX_SATELLITE_MAP_STYLE},
+          {name: 'Maplibre Demotile Globe', mapStyle: 'https://demotiles.maplibre.org/globe.json'},
+          {name: 'OpenMap Tiles OSM Bright', mapStyle: 'https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json'}
+        ]}
+      />
     </div>
   );
 };
