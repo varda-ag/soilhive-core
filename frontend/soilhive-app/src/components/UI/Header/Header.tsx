b82724a996d857d1c721a5167bc5d186b6b2be5c
VardAlb <alberto.arpini@varda.ag>
2025-12-01 16:43:20 +0100

    SP-4668 added navigation buttons

--- frontend/soilhive-app/src/components/UI/Header/Header.tsx
+++ frontend/soilhive-app/src/components/UI/Header/Header.tsx
@@ -1,17 +1,25 @@
 import useTheme from "../../../hooks/useTheme";
+import { singlePages } from "../../../utilities/moduleFederation";
+import { Button } from "../Button/Button";
 import styles from './Header.module.scss'
 
-export default function Header(){
-    
-    const {logo} = useTheme()
+export default function Header() {
+
+    const { logo, theme } = useTheme()
+
+    if (!theme) { // Can this really happen?
+        return null;
+    }
 
     return (
         <header className={styles.header}>
             <div className={styles.header}>
-                {logo && <img src={logo} alt="logo" style={{width: '167px', height: '59px'}} />}
+                {logo && <img src={logo} alt="logo" style={{ width: '167px', height: '59px' }} />}
             </div>
             <nav className={styles.nav}>
-
+                <Button to="/">Availability</Button>
+                {singlePages.map(({ route, name }) => <Button key={route} to={`/${route}`}>{name}</Button>)}
+                <Button to='/Admin'>Admin</Button>
             </nav>
         </header>
     )
