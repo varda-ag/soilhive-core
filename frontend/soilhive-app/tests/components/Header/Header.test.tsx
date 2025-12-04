4f0f1aee8855f5177842cd1c39635347cd013348
VardAlb <alberto.arpini@varda.ag>
2025-12-04 10:26:59 +0100

    SP-4668 UI header (#31)
    * SP-4668 header basic structure
    
    * SP-4668 added navigation buttons
    
    * SP-4668 moved header to component folder
    
    * SP-4668 added login button
    
    * #SP-4668 re-arranged main components in heade
    
    * SP-4668 selected menu item underline
    
    * SP-4668 fixed img logo heigh
    
    * SP-4668 added legal page placeholder
    
    * SP-4668 increased space between text and underline in navigation menu
    
    * SP-4668 minor menu item underline tweak
    
    * SP-4668 mobile draft
    
    * SP-4668 fixed user icon coloring
    
    * SP-4668 first header test
    
    * SP-4668 simplified mock import
    
    * SP-4668 cleaned up code
    
    * SP-4668 additional cleanup
    
    * SP-4668 removed unused tag to avoid react warning
    
    * SP-4668 extended test when authenticated
    
    * SP-4668 added a test for hamburger menu
    
    * SP-4668 used hamburger icon instead of three spans
    
    * SP-4668 renamed hamburger menu item
    
    * SP-4668 exapand / collapse toggle button
    
    * SP-4668 moved mobile menu to its sub component
    
    * SP-4668 moved mobile menu to its component
    
    * SP-4668 fixed mobile menu visibility
    
    * SP-4668 added mobile menu test
    
    * SP-4668 fixed hamburger menu visibility in desktop mode

---
+++ frontend/soilhive-app/tests/components/Header/Header.test.tsx
@@ -0,0 +1,72 @@
+import { render, screen } from '@testing-library/react';
+
+jest.mock('../../../src/auth/AuthContextProvider', () => ({
+    useAuthContext: jest.fn()
+}))
+import { useAuthContext } from '../../../src/auth/AuthContextProvider';
+
+jest.mock('../../../src/utilities/moduleFederation', () => ({
+    singlePages: []
+}))
+
+jest.mock('../../../src/hooks/useTheme', () => ({
+    __esModule: true,
+    default: jest.fn()
+}))
+import useTheme from '../../../src/hooks/useTheme'
+
+import Header from 'components/Header/Header'
+
+jest.mock('react-router', () => ({
+    NavLink: ({ to, children }: any) => (
+        <a href={to} >
+            {children}
+        </a>
+    )
+}))
+
+jest.mock('assets/icons/small-user-icon.svg?react', () => ({
+    __esModule: true,
+    default: () => <svg data-testid="user-icon" />,
+}));
+
+describe('Header component', () => {
+    it('snapshot with authenticated user', () => {
+
+        (useAuthContext as jest.Mock).mockReturnValue({
+            isAuthenticated: true,
+            isLoading: false,
+            login: jest.fn(),
+            logout: jest.fn(),
+        })
+
+        const mockUseTheme = useTheme as jest.Mock
+        mockUseTheme.mockReturnValue({
+            logo: 'logo.png'
+        })
+
+        const { container } = render(<Header />)
+        const btn = screen.getByTestId('sh-ui-button');
+        expect(btn).toHaveTextContent('Log out')
+        expect(container).toMatchSnapshot()
+    })
+
+    it('renders hamburger menu button', () => {
+        (useAuthContext as jest.Mock).mockReturnValue({
+            isAuthenticated: false,
+            isLoading: false,
+            login: jest.fn(),
+            logout: jest.fn(),
+        })
+
+        const mockUseTheme = useTheme as jest.Mock
+        mockUseTheme.mockReturnValue({
+            logo: 'logo.png'
+        })
+
+        render(<Header />)
+
+        const hamburger = screen.getByRole('button', { name: /menu/i })
+        expect(hamburger).toBeInTheDocument()
+    })
+})
