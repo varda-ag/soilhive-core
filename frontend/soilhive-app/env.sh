7a2de0e2c2fc9af3df7fa50af5d8b2cb77217e10
svaccari <stefano.vaccari@varda.ag>
2025-12-12 09:12:32 +0100

    Using absolute paths in startup script

--- frontend/soilhive-app/env.sh
+++ frontend/soilhive-app/env.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cat << EOF > ./env-config.js
+OUTPUT_FOLDER=${1:-public}
+mkdir -p $OUTPUT_FOLDER
+cat << EOF > $OUTPUT_FOLDER/env-config.js
 window._env_ = {
   BACKEND_BASE_URL: '${BACKEND_BASE_URL}',
   MAPBOX_ACCESS_TOKEN: '${MAPBOX_ACCESS_TOKEN}',
