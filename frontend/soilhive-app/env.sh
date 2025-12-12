2901252726dbaa4a4225ef621eeced336e2fd5ac
svaccari <stefano.vaccari@varda.ag>
2025-12-12 08:27:08 +0100

    Fixing env.sh for nginx (#53)

--- frontend/soilhive-app/env.sh
+++ frontend/soilhive-app/env.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mkdir -p public
-cat << EOF > ./public/env-config.js
+cat << EOF > ./env-config.js
 window._env_ = {
   BACKEND_BASE_URL: '${BACKEND_BASE_URL}',
   MAPBOX_ACCESS_TOKEN: '${MAPBOX_ACCESS_TOKEN}',
