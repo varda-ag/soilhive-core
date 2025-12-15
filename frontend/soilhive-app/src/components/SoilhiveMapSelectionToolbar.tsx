7634cf04c7fb551ef48cb5918e6c7c2707c1c4de
cristbello <cristian.bellomo@varda.ag>
2025-12-15 11:22:21 +0100

    feat: Changed icons to use current color, removed one and removed info icon from area

--- frontend/soilhive-app/src/components/SoilhiveMapSelectionToolbar.tsx
+++ frontend/soilhive-app/src/components/SoilhiveMapSelectionToolbar.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable */
-import ResetIcon from 'assets/icons/reset-gray-icon.svg?react';
-import MarkerIcon from 'assets/icons/marker-white-icon.svg?react';
-import PlusIcon from 'assets/icons/plus-green-icon.svg?react';
-import InfoIcon from 'assets/icons/info-gray-icon.svg?react';
+import ResetIcon from 'assets/icons/small-reset-icon.svg?react';
+import MarkerIcon from 'assets/icons/small-marker-icon.svg?react';
+import PlusIcon from 'assets/icons/small-plus-icon.svg?react';
 import { Button } from './UI';
 
 interface SoilhiveMapSelectionToolbarProps {
@@ -16,11 +15,11 @@ interface SoilhiveMapSelectionToolbarProps {
 export default function SoilhiveMapSelectionToolbar({ area = 0, onCancel, onReset, onDrawAnother, onShowResults }: SoilhiveMapSelectionToolbarProps) {
   return (
     <div className="soilhive-map-selection-toolbar">      
-      <Button size="tiny" type="tertiary" onClick={onReset}><ResetIcon />Reset</Button>
+      <Button size="tiny" type="tertiary" onClick={onReset} className="reset-button"><ResetIcon />Reset</Button>
       <Button size="tiny" type="tertiary" onClick={onCancel}>Cancel</Button>
       <Button size="tiny" type="secondary" onClick={onDrawAnother} isDisabled={true}><PlusIcon />Draw another area</Button>
       <Button size="tiny" type="primary" onClick={onShowResults}><MarkerIcon />Show results</Button>
-      <div className="selection-area"><span className="area" title={`${area} km²`}>{`${area}`} km²</span><InfoIcon /></div>
+      <div className="selection-area">{area} km²</div>
     </div>
   );
 }
