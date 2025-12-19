f091c81c0720637fbc493f3cdad88abe9bcd2469
cristbello <153092107+cristbello@users.noreply.github.com>
2025-12-19 12:55:49 +0100

    SP-4728 Map drawing and shape upload: whenever a shape is selected a popup must be shown in the center of mass of the biggest geometry in that shape like we do for geocoding (#77)

--- frontend/soilhive-app/src/components/SoilhiveMap.jsx
+++ frontend/soilhive-app/src/components/SoilhiveMap.jsx
@@ -7,8 +7,8 @@ import '@watergis/maplibre-gl-terradraw/dist/maplibre-gl-terradraw.css'
 import '../styles/SoilhiveMap.scss';
 import Flower from '../assets/images/flower.svg?react';
 import { polygonToCells } from 'h3-js';
-import { bBoxToH3Cells, h3IndexesToGeoJSONPolygons, isPointInFeatureCollection } from '../utilities/geo';
-import { area, bbox, bboxPolygon, convertArea, round } from '@turf/turf';
+import { bBoxToH3Cells, h3IndexesToGeoJSONPolygons, isPointInFeatureCollection, largestPolygonInsideMultipolygon as largestPolygonInsideMultipolygonFn } from '../utilities/geo';
+import { area, bbox as bboxFn, bboxPolygon, centerOfMass, convertArea, round } from '@turf/turf';
 import { h3ResolutionForZoomLevel } from '../utilities/map';
 import DrawControl from './DrawControl';
 import SoilhiveMapToolbar from './SoilhiveMapToolbar';
@@ -228,8 +228,23 @@ function SoilhiveMap({
                 type: 'FeatureCollection',
                 features: [geojson]
               });
+              if(geojson?.geometry?.type === 'MultiPolygon') {
+                const largestPolygonInsideMultiPolygon = largestPolygonInsideMultipolygonFn(geojson);
+                console.assert(largestPolygonInsideMultiPolygon !== null, 'A valid MultiPolygon should contain at least a Polygon');
+                // Used center of mass so it's guaranteed to be withing the polygon (good for concave shapes)
+                // if we use centroid for the geometric center it might fall outside concave shapes
+                const center = centerOfMass(largestPolygonInsideMultiPolygon);
+                const [lng, lat] = center.geometry.coordinates;                
+                const bbox = bboxFn(largestPolygonInsideMultiPolygon);
+                mapRef.current.fitBounds(bbox, { padding: 40 });
+                setSelectedPoint({ lng, lat });
+              } else {
+                const center = centerOfMass(geojson);
+                const [lng, lat] = center.geometry.coordinates;       
+                mapRef.current.fitBounds(bboxFn(geojson), { padding: 40 });
+                setSelectedPoint({ lng, lat });
+              }
               setShowSelectionToolbar(true);
-              mapRef.current.fitBounds(bbox(geojson), { padding: 40 });
             }}
           />
         </Activity>        
@@ -326,6 +341,14 @@ function SoilhiveMap({
                 type: 'FeatureCollection',
                 features: [feature]
               });
+              const center = centerOfMass(feature);
+              const [lng, lat] = center.geometry.coordinates;
+              // Using a state and a ref to keep track of when drawing started and ended didn't work
+              // to prevent the popup showing into the last point you clicked on to close the drawn shape
+              // so I use this timeout trick so that the popup appears in the correct center position.
+              setTimeout(() => {
+                setSelectedPoint({ lng, lat });
+              }, 0);
             }}
           />  
         }
