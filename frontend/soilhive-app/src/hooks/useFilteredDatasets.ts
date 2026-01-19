0cbc849ec10b43c353feb5b38a2b420dad5a5f67
svaccari <stefano.vaccari@varda.ag>
2026-01-19 10:17:56 +0100

    Moving useDebounce into another file

--- frontend/soilhive-app/src/hooks/useFilteredDatasets.ts
+++ frontend/soilhive-app/src/hooks/useFilteredDatasets.ts
@@ -1,22 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+import type { DatasetFilter, PostDatasetFilterResponse } from 'types/backend';
 import { useRequest } from '../api-client';
 import { BACKEND_BASE_URL } from '../configuration/api';
-import type { DatasetFilter, PostDatasetFilterResponse } from 'types/backend';
-import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useDebounce } from './useDebounce';
 
 export function useFilteredDatasets(filters: DatasetFilter) {
   const { request } = useRequest();
   const debouncedFilters = useDebounce(filters, 300);
-
-  // Simple useDebounce hook
-  function useDebounce<T>(value: T, delay: number): T {
-    const [debouncedValue, setDebouncedValue] = useState(value);
-    useEffect(() => {
-      const handler = setTimeout(() => setDebouncedValue(value), delay);
-      return () => clearTimeout(handler);
-    }, [value, delay]);
-    return debouncedValue;
-  }
 
   const fetchFilteredDatasets = async (filter?: DatasetFilter): Promise<PostDatasetFilterResponse> => {
     const res = await request({
