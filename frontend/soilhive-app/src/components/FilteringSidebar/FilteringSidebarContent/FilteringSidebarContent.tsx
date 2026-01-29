2c2811f027b1f1129665fe3d4fcb9523e7b2e185
Oleksii Leontiev <oleksii.leontiev@varda.ag>
2026-01-29 14:16:54 +0100

    feat: sp-4823 - remove unsupported ui elements

--- frontend/soilhive-app/src/components/FilteringSidebar/FilteringSidebarContent/FilteringSidebarContent.tsx
+++ frontend/soilhive-app/src/components/FilteringSidebar/FilteringSidebarContent/FilteringSidebarContent.tsx
@@ -1,7 +1,6 @@
 import { Accordion } from 'components/UI';
 import { FilteringSidebarDataScope } from '../FilteringSidebarDataScope/FilteringSidebarDataScope';
 import { FilteringSidebarParameters } from '../FilteringSidebarParameters/FilteringSidebarParameters';
-import { FilteringSidebarLandEcosystem } from '../FilteringSidebarLandEcosystem/FilteringSidebarLandEcosystem';
 
 import styles from './FilteringSidebarContent.module.scss';
 
@@ -13,9 +12,6 @@ export function FilteringSidebarContent() {
       </Accordion>
       <Accordion title="Soil parameters" openedFromStart={true}>
         <FilteringSidebarParameters />
-      </Accordion>
-      <Accordion title="Land & ecosystem" openedFromStart={true}>
-        <FilteringSidebarLandEcosystem />
       </Accordion>
     </div>
   );
