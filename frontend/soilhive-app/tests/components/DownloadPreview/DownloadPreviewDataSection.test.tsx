355dbe0d16647fd0afdc08f835fc7bfc9556b044
cristbello <153092107+cristbello@users.noreply.github.com>
2026-02-03 12:41:08 +0100

    feat: sp-4735 added filtering support to preview page (#157)
    Desktop
    <img width="1850" height="1489" alt="Screenshot 2026-02-03 at 10 37 58"
    src="https://github.com/user-attachments/assets/b3a9585a-4f52-4f45-8bec-9ae54e4a2caa"
    />
    Tablet
    <img width="676" height="1054" alt="Screenshot 2026-02-03 at 10 39 12"
    src="https://github.com/user-attachments/assets/62d76a0c-7b7a-4868-9227-e842bda37a05"
    />
    Mobile
    <img width="693" height="840" alt="Screenshot 2026-02-03 at 10 46 11"
    src="https://github.com/user-attachments/assets/df3706ce-331e-4841-a95c-3ece47a3625e"
    />
    <img width="671" height="842" alt="Screenshot 2026-02-03 at 10 38 39"
    src="https://github.com/user-attachments/assets/1f15b214-e7af-49d0-bc08-fce84964d8d2"
    />
    Controls
    <img width="691" height="833" alt="Screenshot 2026-02-03 at 10 47 26"
    src="https://github.com/user-attachments/assets/2fbf711d-b8f9-4c0f-ab53-f84f48202bdc"
    />
    <img width="689" height="827" alt="Screenshot 2026-02-03 at 10 47 37"
    src="https://github.com/user-attachments/assets/07ab8092-14ba-4bed-bc5f-44f15ff4847e"
    />

--- frontend/soilhive-app/tests/components/DownloadPreview/DownloadPreviewDataSection.test.tsx
+++ frontend/soilhive-app/tests/components/DownloadPreview/DownloadPreviewDataSection.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import DownloadPreviewDataSection from 'components/DownloadPreview/DownloadPreviewDataSection/DownloadPreviewDataSection';
 
 jest.mock('components/DownloadPreview/DownloadPreviewFilters/DownloadPreviewFilters', () => {
@@ -13,7 +13,17 @@ jest.mock('components/DownloadPreview/DownloadPreviewTable/DownloadPreviewTable'
 
 describe('DownloadPreviewDataSection', () => {
   it('renders the download preview data section', () => {
-    const { container } = render(<DownloadPreviewDataSection />);
+    const { container, getByTestId } = render(<DownloadPreviewDataSection />);
     expect(container).toMatchSnapshot();
+    const filtersButton = getByTestId('download-preview-data-section-filters-button');
+    expect(filtersButton.classList.contains('Secondary')).toBe(true);
+  });
+
+  it('changes the button type when clicking on it', async () => {
+    const { container, getByTestId } = render(<DownloadPreviewDataSection />);
+    expect(container).toMatchSnapshot();
+    const filtersButton = getByTestId('download-preview-data-section-filters-button');
+    await act(async () => filtersButton.click());
+    expect(filtersButton.classList.contains('Secondary')).toBe(false);
   });
 });
--- frontend/soilhive-app/tests/components/DownloadPreview/DownloadPreviewFilters.test.tsx
+++ frontend/soilhive-app/tests/components/DownloadPreview/DownloadPreviewFilters.test.tsx
@@ -1,9 +1,57 @@
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import DownloadPreviewFilters from 'components/DownloadPreview/DownloadPreviewFilters/DownloadPreviewFilters';
+import { __setIsMobileLayout } from 'hooks/useDevice';
+
+jest.mock('primereact/dropdown', () => {
+  const Dropdown = () => <div>Mock Dropdown</div>;
+  return { Dropdown };
+});
+
+jest.mock('primereact/calendar', () => {
+  const Calendar = () => <div>Mock Calendar</div>;
+  return { Calendar };
+});
+
+jest.mock('primereact/dialog', () => {
+  const Dialog = ({ children }: any) => <div>Mock Dialog {children}</div>;
+  return { Dialog };
+});
+
+jest.mock('hooks/useDevice');
 
 describe('DownloadPreviewFilters', () => {
-  it('renders the download preview filters', () => {
-    const { container } = render(<DownloadPreviewFilters />);
+  beforeEach(() => {
+    __setIsMobileLayout(false);
+  });
+
+  it('renders the download preview filters in tablet and desktop', () => {
+    const { container, queryByText } = render(<DownloadPreviewFilters />);
     expect(container).toMatchSnapshot();
+    expect(queryByText('Mock Dialog')).not.toBeInTheDocument();
+  });
+
+  it('renders the download preview filters in mobile', () => {
+    __setIsMobileLayout(true);
+    const { container, queryByText } = render(<DownloadPreviewFilters />);
+    expect(container).toMatchSnapshot();
+    expect(queryByText('Mock Dialog')).toBeInTheDocument();
+  });
+
+  it('closes the dialog if you click on "apply filters" button', async () => {
+    __setIsMobileLayout(true);
+    const setDialogOpenFn = jest.fn();
+    const { getByText } = render(<DownloadPreviewFilters dialogOpen={true} setDialogOpen={setDialogOpenFn} />);
+    const applyButton = getByText('Apply filters');
+    await act(async () => applyButton.click());
+    expect(setDialogOpenFn).toHaveBeenCalledWith(false);
+  });
+
+  it('does not do anything if you click on "apply filters" button and the dialog is already closed', async () => {
+    __setIsMobileLayout(true);
+    const setDialogOpenFn = jest.fn();
+    const { getByText } = render(<DownloadPreviewFilters dialogOpen={false} setDialogOpen={setDialogOpenFn} />);
+    const applyButton = getByText('Apply filters');
+    await act(async () => applyButton.click());
+    expect(setDialogOpenFn).not.toHaveBeenCalled();
   });
 });
