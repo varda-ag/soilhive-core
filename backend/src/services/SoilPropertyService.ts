e698287bc18d291c6e63a21309c01e4c7be05239
svaccari <stefano.vaccari@varda.ag>
2026-02-09 14:21:15 +0100

    fix(data): replacing IDs with slugs

--- backend/src/services/SoilPropertyService.ts
+++ backend/src/services/SoilPropertyService.ts
@@ -1,7 +1,7 @@
 import { RequestData } from '../interfaces/RequestData';
 import { SoilProperty } from '../interfaces/SoilProperty';
 import SoilPropertyEntity from '../entities/SoilProperty';
-import { getEntity, getEntities } from '../utils/slugs';
+import { getEntity, getEntities, idToSlug } from '../utils/slugs';
 import { EntityType } from '../types/data';
 
 export default class SoilPropertyService {
@@ -16,5 +16,31 @@ export default class SoilPropertyService {
 
   getSoilPropertiesBySlug = async (requestData: RequestData, slugs: string[]): Promise<SoilProperty[]> => {
     return await getEntities(requestData, SoilPropertyEntity, EntityType.SOIL_PROPERTY, slugs);
+  };
+
+  propertiesIdToSlugs = async (requestData: RequestData, input: SoilProperty | SoilProperty[]): Promise<SoilProperty | SoilProperty[]> => {
+    if (Array.isArray(input)) {
+      const idMap = input.reduce(
+        (acc, item) => {
+          acc[item.id] = item;
+          return acc;
+        },
+        {} as Map<string, SoilPropertyEntity>,
+      );
+      for (const item of input) {
+        if (item.parent_property_id) {
+          // Replacing the parent_property_id with the corresponding slug
+          item.parent_property_id = idMap[item.parent_property_id].slug;
+        }
+      }
+      return idToSlug(input);
+    }
+    if (!input.parent_property_id) {
+      return idToSlug(input);
+    }
+    const repo = requestData.entityManager.getRepository(SoilPropertyEntity);
+    const parent = await repo.findOneByOrFail({ id: input.parent_property_id });
+    input.parent_property_id = parent.slug;
+    return idToSlug(input);
   };
 }
