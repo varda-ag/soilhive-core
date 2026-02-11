bfafc1acdacf9dc93bc4029d04ffcef76922e800
VardAlb <alberto.arpini@varda.ag>
2026-02-11 07:21:29 +0100

    chore(be): sp-4755 cumulative fixes for data management portal (#195)
    This PR include a series of cumulative fixes:
    
    - dataset and mapping endpoints now return 201 (created) on entity
    creation
    - column dataset_id in table DatasetFileMapping has now a not null
    constraint
    - dataset-mapping-file endpoint now return a 409 (conflict) in case a
    duplicated record is inserted
    - all the related tests

---
+++ backend/tests/routes/dataset-file-mapping.test.ts
@@ -0,0 +1,154 @@
+import { describe, it, expect } from '@jest/globals';
+import { getDataAdminToken } from '../helper';
+import request from 'supertest';
+import { app } from '../../src/app';
+import { StatusCodes } from 'http-status-codes';
+import { addFile } from '../../src/utils/mock';
+
+describe('Testing /datasets{datasetId}/dataset-file-mapping routes', () => {
+  describe('POST /datasets{datasetId}/dataset-file-mapping', () => {
+    it('should create an empty dataset file mapping successfully (201)', async () => {
+      const token = await getDataAdminToken();
+
+      const datasetPayload = {
+        name: 'New Unique Dataset',
+        full_name: 'The Full Name Example',
+      };
+
+      const dataset = await request(app).post('/datasets').set('Authorization', `Bearer ${token}`).send(datasetPayload);
+
+      const datasetFileMapping = await request(app)
+        .post(`/datasets/${dataset.body.id}/dataset-file-mapping`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({});
+
+      expect(datasetFileMapping.statusCode).toBe(StatusCodes.CREATED);
+      expect(datasetFileMapping.body.id).toBeDefined();
+    });
+
+    it('should create a dataset file mapping with mappingId (201)', async () => {
+      const token = await getDataAdminToken();
+
+      const datasetPayload = {
+        name: 'New Unique Dataset',
+        full_name: 'The Full Name Example',
+      };
+
+      const dataset = await request(app).post('/datasets').set('Authorization', `Bearer ${token}`).send(datasetPayload);
+
+      const mappingsPayload = {
+        iron: {
+          property_id: 'iron',
+          conversion_id: 'mg/kg',
+        },
+        upper_depth: 'top',
+      };
+
+      const mapping = await request(app).post('/mappings').set('Authorization', `Bearer ${token}`).send(mappingsPayload);
+
+      const datasetFileMappingPayload = {
+        mappingId: `${mapping.body.id}`,
+      };
+
+      const datasetFileMapping = await request(app)
+        .post(`/datasets/${dataset.body.id}/dataset-file-mapping`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(datasetFileMappingPayload);
+
+      expect(datasetFileMapping.statusCode).toBe(StatusCodes.CREATED);
+      expect(datasetFileMapping.body.mappingId).toBe(mapping.body.id);
+    });
+
+    it('should return 409 when creating a duplicate dataset file mapping', async () => {
+      const token = await getDataAdminToken();
+
+      const datasetPayload = {
+        name: 'New Unique Dataset',
+        full_name: 'The Full Name Example',
+      };
+
+      const dataset = await request(app).post('/datasets').set('Authorization', `Bearer ${token}`).send(datasetPayload);
+
+      const mappingsPayload = {
+        iron: {
+          property_id: 'iron',
+          conversion_id: 'mg/kg',
+        },
+        upper_depth: 'top',
+      };
+
+      const mapping = await request(app).post('/mappings').set('Authorization', `Bearer ${token}`).send(mappingsPayload);
+
+      const file = await addFile('test-file');
+
+      const datasetFileMappingPayload = {
+        mappingId: mapping.body.id,
+        fileID: file.id,
+      };
+
+      // Create the first mapping successfully
+      const firstMapping = await request(app)
+        .post(`/datasets/${dataset.body.id}/dataset-file-mapping`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(datasetFileMappingPayload);
+
+      expect(firstMapping.statusCode).toBe(StatusCodes.CREATED);
+
+      // Attempt to create a duplicate mapping with the same dataset, file, and mapping IDs
+      const duplicateMapping = await request(app)
+        .post(`/datasets/${dataset.body.id}/dataset-file-mapping`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(datasetFileMappingPayload);
+
+      expect(duplicateMapping.statusCode).toBe(StatusCodes.CONFLICT);
+    });
+  });
+
+  describe('PATCH /datasets{datasetId}/dataset-file-mapping', () => {
+    it('should update a dataset file mapping (200)', async () => {
+      const token = await getDataAdminToken();
+
+      const datasetPayload = {
+        name: 'New Unique Dataset',
+        full_name: 'The Full Name Example',
+      };
+
+      const dataset = await request(app).post('/datasets').set('Authorization', `Bearer ${token}`).send(datasetPayload);
+
+      const mappingsPayload = {
+        iron: {
+          property_id: 'iron',
+          conversion_id: 'mg/kg',
+        },
+        upper_depth: 'top',
+      };
+
+      const mapping = await request(app).post('/mappings').set('Authorization', `Bearer ${token}`).send(mappingsPayload);
+
+      // create with just the mapping id
+      const datasetFileMapping = await request(app)
+        .post(`/datasets/${dataset.body.id}/dataset-file-mapping`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          mappingId: mapping.body.id,
+        });
+
+      expect(datasetFileMapping.statusCode).toBe(StatusCodes.CREATED);
+      expect(datasetFileMapping.body.mappingId).toBe(mapping.body.id);
+
+      const file = await addFile('test-file');
+
+      // add the file id
+      const datasetFileMappingUpdate = await request(app)
+        .patch(`/datasets/${dataset.body.id}/dataset-file-mapping/${datasetFileMapping.body.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          fileID: file.id,
+        });
+
+      expect(datasetFileMappingUpdate.statusCode).toBe(StatusCodes.OK);
+      expect(datasetFileMappingUpdate.body.mappingId).toBe(mapping.body.id);
+      expect(datasetFileMappingUpdate.body.fileID).toBe(file.id);
+    });
+  });
+});
