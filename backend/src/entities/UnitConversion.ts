45f6140f3be4e60b3e1671352f136842225154b5
Daria Ruiz-Ogarrio Marassanova <daria.marassanova@varda.ag>
2026-02-16 16:37:41 +0100

    feat: procedures and unit_conversions fields update

--- backend/src/entities/UnitConversion.ts
+++ backend/src/entities/UnitConversion.ts
@@ -1,7 +1,8 @@
-import { Entity, Column, PrimaryColumn, Unique, ForeignKey } from 'typeorm';
+import { Entity, Column, Unique, PrimaryColumn, ManyToOne, JoinColumn, ForeignKey } from 'typeorm';
 import { UnitConversion } from '../interfaces/UnitConversion';
 import BaseTable from './BaseTable';
 import SlugHistoryEntity from './SlugHistory';
+import SoilPropertyEntity from './SoilProperty';
 
 @Entity('unit_conversions')
 @Unique(['slug'])
@@ -17,11 +18,18 @@ export default class UnitConversionEntity extends BaseTable implements UnitConve
   @Column({ type: 'text' })
   slug: string;
 
-  @Column({ type: 'text', nullable: true })
-  original_unit_of_measurement?: string;
+  @Column({ type: 'uuid' })
+  property_id: string;
+
+  @ManyToOne(() => SoilPropertyEntity, soil_property => soil_property.id, {
+    deferrable: 'INITIALLY DEFERRED',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'property_id' })
+  soil_property: SoilPropertyEntity;
 
   @Column({ type: 'text', nullable: true })
-  standard_unit?: string;
+  original_unit_of_measurement?: string;
 
   @Column({ type: 'text', nullable: true })
   conversion_formula?: string;
--- backend/src/services/UnitConversionService.ts
+++ backend/src/services/UnitConversionService.ts
@@ -1,13 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
 import { RequestData } from '../interfaces/RequestData';
 import { UnitConversion } from '../interfaces/UnitConversion';
 import UnitConversionEntity from '../entities/UnitConversion';
 import { getEntity, getEntities } from '../utils/slugs';
+import { ErrorResponse } from '../utils/error';
 import { EntityType } from '../types/data';
+import SoilPropertyEntity from '../entities/SoilProperty';
 
 export default class UnitConversionService {
-  getUnitConversions = async (requestData: RequestData): Promise<UnitConversion[]> => {
-    const repo = requestData.entityManager.getRepository(UnitConversionEntity);
-    return await repo.find();
+  getUnitConversions = async (requestData: RequestData, soilPropertyId: string): Promise<UnitConversion[]> => {
+    const { entityManager } = requestData;
+
+    // the id coming in as parameter is the slug that allow us to load the soil_property with it's actual uuid
+    const soilProperty = await getEntity(requestData, SoilPropertyEntity, EntityType.SOIL_PROPERTY, soilPropertyId);
+
+    const repo = entityManager.getRepository(UnitConversionEntity);
+
+    // Find all unit conversions for the soil property
+    const unitConversions = await repo.find({
+      where: {
+        property_id: soilProperty.id,
+      },
+    });
+
+    if (unitConversions.length === 0) {
+      throw new ErrorResponse(`No unit conversions found for soil property '${soilPropertyId}'`, StatusCodes.NOT_FOUND);
+    }
+
+    return unitConversions;
   };
 
   getUnitConversion = async (requestData: RequestData, slug: string): Promise<UnitConversion> => {
