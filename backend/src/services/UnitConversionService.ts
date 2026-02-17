71515c08206692b19d7f9a2967b0bef78847a632
Daria Ruiz-Ogarrio Marassanova <daria.marassanova@varda.ag>
2026-02-17 10:24:36 +0100

    fix: pr comments

--- backend/src/services/UnitConversionService.ts
+++ backend/src/services/UnitConversionService.ts
@@ -1,31 +1,27 @@
-import { StatusCodes } from 'http-status-codes';
 import { RequestData } from '../interfaces/RequestData';
 import { UnitConversion } from '../interfaces/UnitConversion';
 import UnitConversionEntity from '../entities/UnitConversion';
 import { getEntity, getEntities } from '../utils/slugs';
-import { ErrorResponse } from '../utils/error';
 import { EntityType } from '../types/data';
 import SoilPropertyEntity from '../entities/SoilProperty';
 
 export default class UnitConversionService {
-  getUnitConversions = async (requestData: RequestData, soilPropertyId: string): Promise<UnitConversion[]> => {
+  getUnitConversions = async (requestData: RequestData, soilPropertySlug?: string): Promise<UnitConversion[]> => {
     const { entityManager } = requestData;
 
-    // the id coming in as parameter is the slug that allow us to load the soil_property with it's actual uuid
-    const soilProperty = await getEntity(requestData, SoilPropertyEntity, EntityType.SOIL_PROPERTY, soilPropertyId);
+    const whereConditions: any = {};
+
+    if (soilPropertySlug !== undefined) {
+      const soilProperty = await getEntity(requestData, SoilPropertyEntity, EntityType.SOIL_PROPERTY, soilPropertySlug);
+      whereConditions.property_id = soilProperty.id;
+    }
 
     const repo = entityManager.getRepository(UnitConversionEntity);
 
     // Find all unit conversions for the soil property
     const unitConversions = await repo.find({
-      where: {
-        property_id: soilProperty.id,
-      },
+      where: whereConditions,
     });
-
-    if (unitConversions.length === 0) {
-      throw new ErrorResponse(`No unit conversions found for soil property '${soilPropertyId}'`, StatusCodes.NOT_FOUND);
-    }
 
     return unitConversions;
   };
