735dbd5c3eb1ae13aecc454fbf4c982f2d2c45af
Oleksii Leontiev <oleksii.leontiev@varda.ag>
2026-02-23 14:02:53 +0100

    feat: sp-4893 - filtering sidebar accordions changes

--- frontend/soilhive-app/tests/components/FilteringSidebar/FilteringSidebarContent/FilteringSidebarContent.test.tsx
+++ frontend/soilhive-app/tests/components/FilteringSidebar/FilteringSidebarContent/FilteringSidebarContent.test.tsx
@@ -1,15 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { FilteringSidebarContent } from 'components/FilteringSidebar/FilteringSidebarContent/FilteringSidebarContent';
 
-jest.mock('components/UI', () => ({
-  Accordion: ({ title, children }: { title: string; children: React.ReactNode }) => (
-    <div data-testid="accordion">
-      <div data-testid="accordion-title">{title}</div>
-      <div data-testid="accordion-content">{children}</div>
-    </div>
-  ),
-}));
-
 jest.mock('components/FilteringSidebar/FilteringSidebarParameters/FilteringSidebarParameters', () => ({
   FilteringSidebarParameters: () => <div data-testid="mock-filtering-sidebar-parameters">Mock FilteringSidebarParameters</div>,
 }));
@@ -19,11 +10,11 @@ jest.mock('components/FilteringSidebar/FilteringSidebarDataScope/FilteringSideba
 }));
 
 describe('FilteringSidebarContent', () => {
-  it('renders three accordion components', () => {
+  it('renders two sections with child components', () => {
     const { container } = render(<FilteringSidebarContent />);
 
-    const accordions = screen.getAllByTestId('accordion');
-    expect(accordions).toHaveLength(2);
+    const sections = screen.getAllByTestId('sh-filtering-sidebar-section');
+    expect(sections).toHaveLength(2);
     expect(screen.getByTestId('mock-filtering-sidebar-parameters')).toBeInTheDocument();
     expect(screen.getByTestId('mock-filtering-sidebar-datascope')).toBeInTheDocument();
     expect(container).toMatchSnapshot();
