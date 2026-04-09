e5e2db64f43bfc10a3fe48238006e12b40686a6d
Daria Ruiz-Ogarrio Marassanova <daria.marassanova@varda.ag>
2026-04-09 12:03:11 +0200

    fix: updated unit conversion trigger function

--- backend/src/migrations/1775121569960-Entitlements.ts
+++ backend/src/migrations/1775121569960-Entitlements.ts
@@ -67,58 +67,58 @@ export class Entitlements1775121569960 implements MigrationInterface {
                                         end;
                                     $function$`);
     await queryRunner.query(`CREATE OR REPLACE FUNCTION slug_procedures_generate_store_old()
-        RETURNS trigger
-        LANGUAGE plpgsql
-        AS $function$
-           declare
-           lv_values_names text[];
-           lv_base_slug text;
-           lv_new_slug TEXT;
-           lv_counter INTEGER := 1;
-           lv_exists boolean;
-           begin
-           -- Generate the base slug
-           SELECT ARRAY[pv1.name, t.technique::text, pv2.name, pv3.name, pv4.name, pv4.name, pv6.name, pv7.name]
-           INTO lv_values_names
-            FROM (SELECT NEW.*) AS t 
-            LEFT JOIN vocabulary pv1 ON NEW.sample_pretreatment_id=pv1.id and pv1.category='sample_pretreatment'
-            LEFT JOIN vocabulary pv2 ON NEW.laboratory_method_id=pv2.id and pv2.category='laboratory_method'
-            LEFT JOIN vocabulary pv3 ON NEW.extractant_concentration_id=pv3.id and pv3.category='extractant_concentration'
-            LEFT JOIN vocabulary pv4 ON NEW.extraction_ratio_id=pv4.id and pv4.category='extraction_ratio'
-            LEFT JOIN vocabulary pv5 ON NEW.extraction_base_id=pv5.id and pv5.category='extraction_base'
-            LEFT JOIN vocabulary pv6 ON NEW.measurement_procedure_id=pv6.id and pv6.category='measurement_procedure'
-            LEFT JOIN vocabulary pv7 ON NEW.limit_of_detection_id=pv7.id and pv7.category='limit_of_detection';
-           
-            EXECUTE format('SELECT %I.slugify(concat_ws(''-'', %L))', TG_TABLE_SCHEMA, array_to_string(lv_values_names, ', ')) into lv_base_slug;
-           lv_new_slug := lv_base_slug;
-
-           EXECUTE format(
-             'SELECT EXISTS (SELECT 1 FROM %I.slug_history WHERE slug = %L)',
-             TG_TABLE_SCHEMA,
-             lv_new_slug
-           ) into lv_exists;
-           -- Check if the slug already exists
-           WHILE lv_exists LOOP
-           -- If it exists, append a number and increment
-               lv_new_slug := lv_base_slug || '-' || lv_counter;
-               lv_counter := lv_counter + 1;
-               EXECUTE format(
-                 'SELECT EXISTS (SELECT 1 FROM %I.slug_history WHERE slug = %L)',
-                 TG_TABLE_SCHEMA,
-                 lv_new_slug
-               ) into lv_exists;
-           END LOOP;
-       
-           NEW.slug := lv_new_slug;
-           -- Schema-qualify slug_history + enum type
-           EXECUTE format(
-             'INSERT INTO %I.slug_history(entity_id, entity_type, slug) VALUES ($1, $2::%I.slug_history_entity_type_enum, $3)',
-             TG_TABLE_SCHEMA,
-             TG_TABLE_SCHEMA
-           ) USING NEW.id, TG_TABLE_NAME, NEW.slug;
-           RETURN NEW;
-           end;
-       $function$`);
+                                RETURNS trigger
+                                LANGUAGE plpgsql
+                                AS $function$
+                                    declare
+                                    lv_values_names text[];
+                                    lv_base_slug text;
+                                    lv_new_slug TEXT;
+                                    lv_counter INTEGER := 1;
+                                    lv_exists boolean;
+                                    begin
+                                    -- Generate the base slug
+                                    SELECT ARRAY[pv1.name, t.technique::text, pv2.name, pv3.name, pv4.name, pv5.name, pv6.name, pv7.name]
+                                    INTO lv_values_names
+                                    FROM (SELECT NEW.*) AS t 
+                                    LEFT JOIN vocabulary pv1 ON COALESCE(NEW.sample_pretreatment_id, OLD.sample_pretreatment_id)=pv1.id and pv1.category='sample_pretreatment'
+                                    LEFT JOIN vocabulary pv2 ON COALESCE(NEW.laboratory_method_id, OLD.laboratory_method_id)=pv2.id and pv2.category='laboratory_method'
+                                    LEFT JOIN vocabulary pv3 ON COALESCE(NEW.extractant_concentration_id, OLD.extractant_concentration_id)=pv3.id and pv3.category='extractant_concentration'
+                                    LEFT JOIN vocabulary pv4 ON COALESCE(NEW.extraction_ratio_id, OLD.extraction_ratio_id)=pv4.id and pv4.category='extraction_ratio'
+                                    LEFT JOIN vocabulary pv5 ON COALESCE(NEW.extraction_base_id, OLD.extraction_base_id)=pv5.id and pv5.category='extraction_base'
+                                    LEFT JOIN vocabulary pv6 ON COALESCE(NEW.measurement_procedure_id, OLD.measurement_procedure_id)=pv6.id and pv6.category='measurement_procedure'
+                                    LEFT JOIN vocabulary pv7 ON COALESCE(NEW.limit_of_detection_id, OLD.limit_of_detection_id)=pv7.id and pv7.category='limit_of_detection';
+                                    
+                                    EXECUTE format('SELECT %I.slugify(concat_ws(''-'', %L))', TG_TABLE_SCHEMA, array_to_string(lv_values_names, ', ')) into lv_base_slug;
+                                    lv_new_slug := lv_base_slug;
+                        
+                                    EXECUTE format(
+                                      'SELECT EXISTS (SELECT 1 FROM %I.slug_history WHERE slug = %L)',
+                                      TG_TABLE_SCHEMA,
+                                      lv_new_slug
+                                    ) into lv_exists;
+                                    -- Check if the slug already exists
+                                    WHILE lv_exists LOOP
+                                    -- If it exists, append a number and increment
+                                        lv_new_slug := lv_base_slug || '-' || lv_counter;
+                                        lv_counter := lv_counter + 1;
+                                        EXECUTE format(
+                                          'SELECT EXISTS (SELECT 1 FROM %I.slug_history WHERE slug = %L)',
+                                          TG_TABLE_SCHEMA,
+                                          lv_new_slug
+                                        ) into lv_exists;
+                                    END LOOP;
+                                
+                                    NEW.slug := lv_new_slug;
+                                    -- Schema-qualify slug_history + enum type
+                                    EXECUTE format(
+                                      'INSERT INTO %I.slug_history(entity_id, entity_type, slug) VALUES ($1, $2::%I.slug_history_entity_type_enum, $3)',
+                                      TG_TABLE_SCHEMA,
+                                      TG_TABLE_SCHEMA
+                                    ) USING NEW.id, TG_TABLE_NAME, NEW.slug;
+                                    RETURN NEW;
+                                    end;
+                                $function$`);
     await queryRunner.query(`CREATE OR REPLACE FUNCTION slug_unit_conversions_generate_store_old()
           RETURNS trigger
           LANGUAGE plpgsql
@@ -288,5 +288,8 @@ export class Entitlements1775121569960 implements MigrationInterface {
            RETURN NEW;
            end;
        $function$`);
+    await queryRunner.query(`CREATE OR REPLACE TRIGGER unit_conversion_slug
+                                             BEFORE INSERT OR update ON unit_conversions
+                                             FOR EACH ROW EXECUTE PROCEDURE slug_generate_store_old('{$1.original_unit_of_measurement,$1.conversion_formula}')`);
   }
 }
