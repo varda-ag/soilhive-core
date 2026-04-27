d5f393a890908cbbc82f62989ffe7c28f02fc1b4
cristbello <cristian.bellomo@varda.ag>
2026-04-27 12:40:37 +0200

    fix: got rid of availability provider in explore and download page to avoid call to /coverage

--- frontend/soilhive-app/src/modules/AvailabilityModule.tsx
+++ frontend/soilhive-app/src/modules/AvailabilityModule.tsx
@@ -1,52 +1,43 @@
-import { Navigate, Outlet, Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import PageTitle from 'components/PageTitle';
 import { AvailabilityProvider } from '../contexts/AvailabilityContext';
 import DownloadSummary from '../pages/DownloadSummary';
 import DownloadPreview from '../pages/DownloadPreview';
 import Availability from '../pages/Availability';
 
-const AvailabilityContextLayout = () => {
-  return (
-    <AvailabilityProvider>
-      <Outlet />
-    </AvailabilityProvider>
-  );
-};
-
 function AvailabilityModule() {
   return (
     <Routes>
-      <Route element={<AvailabilityContextLayout />}>
-        <Route
-          index
-          element={
-            <>
-              <PageTitle title="SoilHive - Home" />
+      <Route
+        index
+        element={
+          <>
+            <PageTitle title="SoilHive - Home" />
+            <AvailabilityProvider>
               <Availability />
-            </>
-          }
-        />
-        {/* TODO: adapt the download preview page to use the router and query parameters */}
-        <Route
-          path="/explore"
-          element={
-            <>
-              <PageTitle title="SoilHive - Data Explorer" />
-              <DownloadPreview />
-            </>
-          }
-        />
-        <Route
-          path="/download"
-          element={
-            <>
-              <PageTitle title="SoilHive - Download Summary" />
-              <DownloadSummary />
-            </>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
+            </AvailabilityProvider>
+          </>
+        }
+      />
+      <Route
+        path="/explore"
+        element={
+          <>
+            <PageTitle title="SoilHive - Data Explorer" />
+            <DownloadPreview />
+          </>
+        }
+      />
+      <Route
+        path="/download"
+        element={
+          <>
+            <PageTitle title="SoilHive - Download Summary" />
+            <DownloadSummary />
+          </>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
