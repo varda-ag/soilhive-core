2769a0d7b30a4f4c9a277e294d9342142d510395
svaccari <stefano.vaccari@varda.ag>
2026-05-11 15:10:04 +0200

    fix: reverting to localStorage

--- frontend/src/auth/AuthContextProvider.tsx
+++ frontend/src/auth/AuthContextProvider.tsx
@@ -39,7 +39,7 @@ export function AuthContextProvider({ children }: { children: React.ReactNode })
         silent_redirect_uri={authConfig.oidcConfig.silentRedirectUri}
         loadUserInfo
         revokeTokensOnSignout
-        userStore={new WebStorageStateStore({ store: window.sessionStorage })}
+        userStore={new WebStorageStateStore({ store: window.localStorage })}
         onSigninCallback={() => {
           const url = new URL(window.location.href);
           window.history.replaceState({}, document.title, url.pathname);
