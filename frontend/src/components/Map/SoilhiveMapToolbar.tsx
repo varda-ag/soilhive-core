0e1c82a15c3babcb3e9a23906b48e5c8c4a158ce
Oleksii <132890554+oleksiivarda@users.noreply.github.com>
2026-07-21 08:19:08 +0200

    feat: sp-4722 - upload geojson modal (#713)
    Popup with file upload field was added to the "Upload polygon" button on
    the map.
    
    <img width="1512" height="535" alt="Screenshot 2026-07-20 at 09 02 13"
    src="https://github.com/user-attachments/assets/2b5245ce-31dc-40ab-b9c2-3d7a3c042f33"
    />
    <img width="1512" height="535" alt="Screenshot 2026-07-20 at 09 02 33"
    src="https://github.com/user-attachments/assets/9b152138-def3-4848-a747-617b1a4decaf"
    />

--- frontend/src/components/Map/SoilhiveMapToolbar.tsx
+++ frontend/src/components/Map/SoilhiveMapToolbar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { useEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import SmallPolygonIcon from 'assets/icons/small-polygon-icon.svg?react';
@@ -7,9 +6,8 @@ import PencilIcon from 'assets/icons/pencil-icon.svg?react';
 import UploadIcon from 'assets/icons/small-upload-icon.svg?react';
 import type { Polygon, MultiPolygon } from 'geojson';
 import useDevice from 'hooks/useDevice';
-import useNotifications from 'hooks/useNotifications';
 import { useTranslation } from 'react-i18next';
-import { parseGeoJSONFile } from 'utilities/parseGeoJSONFile';
+import { UploadPolygonModal } from './UploadPolygonModal/UploadPolygonModal';
 
 interface SoilhiveMapToolbarProps {
   visible: boolean;
@@ -20,12 +18,11 @@ interface SoilhiveMapToolbarProps {
 export default function SoilhiveMapToolbar({ visible, onDrawClick, onUpload }: SoilhiveMapToolbarProps) {
   const { t } = useTranslation('availability');
   const [open, setOpen] = useState(false);
+  const [isUploadOpen, setIsUploadOpen] = useState(false);
   const selectionButtonRef = useRef<HTMLButtonElement>(null);
   const selectionListRef = useRef<HTMLDivElement>(null);
-  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const { isMobileLayout } = useDevice();
-  const { showNotification } = useNotifications();
 
   const onWindowClick = (event: PointerEvent) => {
     const target = event.target as Node;
@@ -38,34 +35,9 @@ export default function SoilhiveMapToolbar({ visible, onDrawClick, onUpload }: S
 
   useEffect(() => {
     window.addEventListener('click', onWindowClick);
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.accept = '.geojson,.json';
-    fileInput.onchange = async function onFileInputChange(event) {
-      const file = (event as any).target?.files?.item(0);
-      if (!file) {
-        showNotification({ id: 'no-file-uploaded', title: 'Upload failed', message: 'No file uploaded' });
-        return;
-      }
 
-      // Resets the fileInput otherwise if you upload again the same file the onchange won't trigger
-      fileInput.value = '';
-
-      const result = await parseGeoJSONFile(file);
-      if (result.error) {
-        showNotification({ id: result.error.id, title: 'Upload failed', message: result.error.message });
-        return;
-      }
-
-      onUpload(result.polygon);
-    };
-    fileInputRef.current = fileInput;
     return () => {
       window.removeEventListener('click', onWindowClick);
-      if (fileInputRef.current) {
-        fileInputRef.current.onchange = null;
-        fileInputRef.current = null;
-      }
     };
   }, []);
 
@@ -100,13 +72,14 @@ export default function SoilhiveMapToolbar({ visible, onDrawClick, onUpload }: S
         <button
           onClick={() => {
             setOpen(false);
-            fileInputRef.current?.click();
+            setIsUploadOpen(true);
           }}
         >
           <UploadIcon />
           {t('map.upload_a_polygon')}
         </button>
       </div>
+      <UploadPolygonModal visible={isUploadOpen} onUpload={onUpload} onClose={() => setIsUploadOpen(false)} />
     </div>
   );
 }
