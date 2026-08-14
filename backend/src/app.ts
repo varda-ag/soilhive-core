a3a22ce110ff8e287777d6371f25c1c35aafef66
Daria Ruiz-Ogarrio Marassanova <daria.marassanova@varda.ag>
2026-08-14 16:45:06 +0200

    fix: test flow without invokation on startup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ import { getEntityManager, initializeSchema } from './utils/data-source';
 import { log } from './utils/logger';
 import { isQueryDebugEnabled, queryDebugMiddleware } from './utils/query-debug';
 import { getServerPort, isJest, setupEnv } from './utils/utils';
-import { syncVocabularies } from './scripts/syncVocabularies';
+// import { syncVocabularies } from './scripts/syncVocabularies';
 
 setupEnv();
 
